4460c46d8c679318b049c11ff0bbbeeb877bd171
davi0011 <davi0011@yahoo.com>
2014-04-10 12:55:15 -0600

    Update and rename Critter to Critter.java

---
+++ Critter.java
@@ -0,0 +1,85 @@
+package evolvingWilds.vinny;
+
+import java.util.Random;
+
+import creature.phenotype.Block;
+import creature.phenotype.Creature;
+import creature.phenotype.Vector3;
+import evolvingWilds.james.BlockBrain;
+
+public class Critter extends Creature
+{
+  Block blockList[];
+  BlockBrain brainList[];
+  Random rand = new Random();
+  float bestFitness;
+  Vector3 forward;
+  Vector3 up;
+  Critter creature;
+
+  public float getFitness()
+  {
+    return bestFitness;
+  }
+
+  public Critter(Block[] body, Vector3 rootForward, Vector3 rootUp, boolean parent)
+  {
+    super(body, rootForward, rootUp);
+    forward = rootForward;
+    up = rootUp;
+    blockList = body;
+    if (parent)
+    {
+      brainList = new BlockBrain[blockList.length];
+
+      for (int i = 0; i < blockList.length; i++)
+      {
+        brainList[i] = new BlockBrain(rand, i);
+      }
+    }
+  }
+
+  public Block[] getBody()
+  {
+    return blockList;
+  }
+
+  public void hillClimb()
+  {
+    int jointChoice = rand.nextInt(getNumberOfBodyBlocks());
+    if(rand.nextInt(4)<1)
+    brainList[jointChoice].changeJoint();
+    else brainList[jointChoice].changeRules();
+    blockList[jointChoice].setJointToParent(brainList[jointChoice].getJoint());
+    creature = new Critter(blockList, forward, up, false);
+    float newFitness = creature.runSimulation();
+    if (newFitness > bestFitness)
+    {
+      bestFitness = newFitness;
+      brainList[jointChoice].confirmChange();
+    } else
+    {
+      brainList[jointChoice].resetJoint();
+    }
+  }
+
+  private float runSimulation()
+  {
+    float curFitness = 0;
+    float newFitness = 0;
+
+    int steps = 0;
+
+    while (steps < 500)
+    {
+      curFitness = advanceSimulation();
+      if (curFitness > newFitness)
+      {
+        newFitness = curFitness;
+        steps = 0;
+      }
+      steps++;
+    }
+    return newFitness;
+  }
+}
