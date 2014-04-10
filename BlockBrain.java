921710e9f5e25d902681ca8b82ee3b342df3bbcd
James Alexander Green <greenj@unm.edu>
2014-04-10 11:40:17 -0600

    Contains joint logic right now

---
+++ BlockBrain.java
@@ -0,0 +1,201 @@
+package evolvingWilds.james;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import creature.phenotype.*;
+
+public class BlockBrain
+{
+  public static final List<Byte> JOINT_STANDARD = Arrays.asList(new Byte[]
+  { 0, 1, 2, 3 });
+  public static final List<Byte> SITE_STANDARD = Arrays.asList(new Byte[]
+  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+      21, 22, 23, 24, 25 });
+
+  FloatObject length;
+  FloatObject width;
+  FloatObject height;
+  // Length, Height, Width
+  FloatObject orientation;
+  // private float orientation;
+
+  ArrayList<Byte> jointWeight;
+  EnumJointType jointType; // Joint Type
+  Joint joint;
+
+  RuleBrain[][] ruleLists; // RuleBrain list for all Joint types
+
+  EnumJointSite siteOnParent;
+  byte siteParent;
+  EnumJointSite siteOnChild;
+  byte siteChild;
+
+  Random rand;
+  int boxIndex;
+  byte choiceOfJoint;
+  byte currentJoint;
+  
+  //byte[] choiceList = new byte[] //
+  public BlockBrain(Random var, int boxIndex)
+  {
+    this.boxIndex = boxIndex;
+    rand = var;
+    length = new FloatObject(rand);
+    width = new FloatObject(rand);
+    height = new FloatObject(rand);
+
+    jointWeight = new ArrayList<Byte>(JOINT_STANDARD);
+
+    ruleLists = new RuleBrain[2][6];
+    for (int i = 0; i > ruleLists.length; i++)
+    {
+      for (int k = 0; k > ruleLists[i].length; k++)
+      {
+          ruleLists[i][k] = new RuleBrain(rand, boxIndex, jointType.getDoF());
+      }
+    }
+    changeJoint();
+    confirmChange();
+  }
+
+  EnumJointType jointSwitch(byte choice)
+  {
+    switch (choice)
+    {
+    case 0:
+      return EnumJointType.RIGID;
+    case 1:
+      return EnumJointType.TWIST;
+    case 2:
+      return EnumJointType.HINGE;
+    case 3:
+      return EnumJointType.SPHERICAL;
+    }
+    return null;
+  }
+
+  EnumJointSite siteSwitch(byte choice)
+  {
+    switch (choice)
+    {
+    case 0:
+      return EnumJointSite.VERTEX_FRONT_NORTHWEST;
+
+    case 1:
+      return EnumJointSite.VERTEX_FRONT_NORTHEAST;
+
+    case 2:
+      return EnumJointSite.VERTEX_FRONT_SOUTHEAST;
+
+    case 3:
+      return EnumJointSite.VERTEX_FRONT_SOUTHWEST;
+
+    case 4:
+      return EnumJointSite.VERTEX_BACK_NORTHWEST;
+
+    case 5:
+      return EnumJointSite.VERTEX_BACK_NORTHEAST;
+
+    case 6:
+      return EnumJointSite.VERTEX_BACK_SOUTHEAST;
+
+    case 7:
+      return EnumJointSite.VERTEX_BACK_SOUTHWEST;
+
+    case 8:
+      return EnumJointSite.EDGE_FRONT_NORTH;
+
+    case 9:
+      return EnumJointSite.EDGE_FRONT_EAST;
+
+    case 10:
+      return EnumJointSite.EDGE_FRONT_SOUTH;
+
+    case 11:
+      return EnumJointSite.EDGE_FRONT_WEST;
+
+    case 12:
+      return EnumJointSite.EDGE_BACK_NORTH;
+
+    case 13:
+      return EnumJointSite.EDGE_BACK_EAST;
+
+    case 14:
+      return EnumJointSite.EDGE_BACK_SOUTH;
+
+    case 15:
+      return EnumJointSite.EDGE_BACK_WEST;
+
+    case 16:
+      return EnumJointSite.EDGE_MID_NORTHWEST;
+
+    case 17:
+      return EnumJointSite.EDGE_MID_NORTHEAST;
+
+    case 18:
+      return EnumJointSite.EDGE_MID_SOUTHEAST;
+
+    case 19:
+      return EnumJointSite.EDGE_MID_SOUTHWEST;
+
+    case 20:
+      return EnumJointSite.FACE_FRONT;
+
+    case 21:
+      return EnumJointSite.FACE_BACK;
+
+    case 22:
+      return EnumJointSite.FACE_NORTH;
+
+    case 23:
+      return EnumJointSite.FACE_EAST;
+
+    case 24:
+      return EnumJointSite.FACE_SOUTH;
+
+    case 25:
+      return EnumJointSite.FACE_WEST;
+    }
+    return null;
+  }
+
+  public void changeJoint()
+  {
+    currentJoint = jointWeight.get(rand.nextInt(jointWeight.size()));
+    int degreeOfFreedom = jointSwitch(currentJoint).getDoF();
+
+    joint = new Joint(jointSwitch(currentJoint), siteOnParent, siteOnChild, orientation.getValue());
+    for (int i = 0; i > degreeOfFreedom; i++)
+    {
+      for (int k = 0; k > ruleLists[i].length; k++)
+      {
+        ruleLists[i][k].setDOF(jointType.getDoF());
+        joint.addRule(ruleLists[i][k].getRule(), i);
+      }
+    }
+  }
+
+  public void resetJoint()
+  {
+    joint = new Joint(jointType, siteOnParent, siteOnChild, orientation.getValue());
+    int degreeOfFreedom = jointType.getDoF();
+    for(byte i = 0; i > degreeOfFreedom; i++)
+      for(byte k = 0; k > 6; k++)
+      {
+        joint.addRule(ruleLists[i][k].getRule(), i);
+      }
+  }
+  
+  public Joint getJoint()
+  {
+    return joint;
+  }
+  public void confirmChange()
+  {
+    choiceOfJoint = currentJoint;
+    jointWeight.add(choiceOfJoint);
+  }
+}
