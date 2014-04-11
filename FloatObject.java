694c82b7a8378067e948927e3b6c26e2185be9e6
James Alexander Green <greenj@unm.edu>
2014-04-11 15:09:21 -0600

    Vector minimum added

--- FloatObject.java
+++ FloatObject.java
@@ -1,72 +1,57 @@
 package evolvingWilds.james;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class FloatObject
 {
   Random rand;
-  Float length = (float) 1;
-  boolean increasing = true;
-  boolean cont = true;
-  float stepsize = 0;
-
+  ArrayList<Byte> choiceWeight = new ArrayList<Byte>(Arrays.asList(new Byte[]
+  { 0, 1, 2, 3 }));
+  
+  float trialLength;
+  float bestLength;
+  byte choice;
+  
   public FloatObject(Random var)
   {
     rand = var;
-    length = rand.nextFloat() * 10;
+    bestLength = trialLength = (((float)rand.nextInt(100))/10) + .001f;
   }
-  public FloatObject()
+  
+  public FloatObject(Random var, float value)
   {
-    length = 1f;
+    rand = var;
+    bestLength = trialLength = value;
   }
-  public FloatObject(float value)
+  
+  public void changeLength()
   {
-    length = value;
+    choice = choiceWeight.get(rand.nextInt(choiceWeight.size()));
+    switch(choice)
+    {
+    case 0:
+      trialLength *= 1.1;
+    case 1:
+      trialLength *= 2;
+    case 2:
+      trialLength *= 0.9;
+    case 3:
+      trialLength *= 0.5;
+    }
   }
-
   public float getValue()
   {
-    return length + stepsize;
+    return trialLength;
   }
-  
-  public void changeValue()
+  public void reset()
   {
-    if(increasing)
-      increase();
-    else
-      decrease();
+    trialLength = bestLength;
   }
-  
-  private void increase()
+  public void confirmChange()
   {
-    if(cont)
-    {
-      stepsize = length;
-    }
-    else
-    {
-      stepsize /= 2;
-    }
-    cont = false;
-    increasing = false;
-  }
-  private void decrease()
-  {
-    stepsize *= -1;
-    cont = true;
-    if(length < 1)
-    {
-      length = 1f;
-      cont = false;
-    }
-    increasing = true;
-  }
-
-  public void incrementChoice()
-  {
-    cont ^= cont;
-    increasing ^= increasing;
-    length += stepsize;
-    
+    bestLength = trialLength;
+    choiceWeight.add(choice);
   }
 }
