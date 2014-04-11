744013933ab5063524b3d7079143baa18ee322a5
James Alexander Green <greenj@unm.edu>
2014-04-10 18:48:24 -0600

    Does stuff now

--- FloatObject.java
+++ FloatObject.java
@@ -5,14 +5,69 @@ import java.util.Random;
 public class FloatObject
 {
   Random rand;
-  FloatObject(Random var)
+  Float length = (float) 1;
+  boolean increasing = true;
+  boolean cont = true;
+  float stepsize = 0;
+
+  public FloatObject(Random var)
   {
     rand = var;
+    length = rand.nextFloat() * 10;
   }
-  public float getValue()
+  public FloatObject()
   {
-    // TODO Auto-generated method stub
-    return rand.nextFloat()*10;
+    length = 1f;
+  }
+  public FloatObject(float value)
+  {
+    length = value;
   }
 
+  public float getValue()
+  {
+    return length;
+  }
+  
+  public void changeValue()
+  {
+    if(increasing)
+      increase();
+    else
+      decrease();
+  }
+  
+  private void increase()
+  {
+    if(cont)
+    {
+      stepsize = length;
+    }
+    else
+    {
+      stepsize /= 2;
+    }
+    length += stepsize;
+    cont = false;
+    increasing = false;
+  }
+  private void decrease()
+  {
+    stepsize /= 2;
+    length -= stepsize;
+    cont = true;
+    if(length < 1)
+    {
+      length = 1f;
+      cont = false;
+    }
+    increasing = true;
+  }
+
+  public void incrementChoice()
+  {
+    cont ^= cont;
+    increasing ^= increasing;
+    
+  }
 }
