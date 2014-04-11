1a4e1b5c2afc5c023c0a520585796080801243e4
Toridekage <Exmachina223@yahoo.com>
2014-04-11 02:11:13 -0600

    Create EnumAlleleReturn.java

---
+++ EnumAlleleReturn.java
@@ -0,0 +1,14 @@
+//Matt Locklin
+//Updated 4/11/14 12:27pm
+package creature.group;
+
+public enum EnumAlleleReturn {
+
+	INT,
+	FLOAT,
+	ENUMJOINTTYPE,
+	ENUMJOINTSITE,
+	ENUMBIN ,
+	ENUMUN ,
+ ENUMNEURONINPUTTYPE
+}
