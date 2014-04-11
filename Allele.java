a51c3d0a77864b1f25a08af0543d8ec1fb88f0bb
Toridekage <Exmachina223@yahoo.com>
2014-04-11 02:00:32 -0600

    Update Allele.java

--- Allele.java
+++ Allele.java
@@ -1,33 +1,27 @@
 //Matt Locklin
-//Updated 4/8/14 10:15pm
+//Updated 4/11/14 12:27pm
 
 package creature.group;
 
+import creature.phenotype.EnumJointSite;
+import creature.phenotype.EnumJointType;
+import creature.phenotype.EnumOperatorBinary;
+import creature.phenotype.EnumOperatorUnary;
 
 public class Allele {
-	
-	private Object value;
-	
+
 	private EnumAlleleType alleletype;
-	
-	
-	public Allele(Object v,EnumAlleleType a){
-		value=v;
-		alleletype=a;
-	}
-		
-	public Object getValue() {
-		return value;
+	private Object value;
+
+	private EnumOperatorUnary EOU_value;
+
+	public Allele(Object v, EnumAlleleType a) {
+		value = v;
+		alleletype = a;
 	}
 
 	public EnumAlleleType getAlleletype() {
 		return alleletype;
 	}
-
-	public void setValue(Object value) {
-		this.value = value;
-	}
-
-	
 
 }
